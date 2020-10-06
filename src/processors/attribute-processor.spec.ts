1f6631748ecb6a1a9a5b707e3a07a6d1ed4c8803
Emrah Oner <emrahoner@gmail.com>
2020-10-06 19:43:13 +1100

    feat: addToParents and append attribute processor

--- src/processors/attribute-processor.spec.ts
+++ src/processors/attribute-processor.spec.ts
@@ -1,18 +1,17 @@
 import HtmlPipeline from "../html-pipeline"
+import { AttributeProcessorParameters } from "./attribute-processor"
 
 describe('AttributeProcessor', () => {
     let html: string
     beforeEach(() => {
-        html = `
-        <html>
+        html = `<html>
         <body>
             <div class="content" attr1></div>
             <span attr1></span>
             <a attr2><img src="../src/sample.jpeg "></a>
             <span attr2></span>
         </body>
-        </html>
-        `
+        </html>`
     })
 
     it('add attribute to elements with a selector', () => {
@@ -58,7 +57,7 @@ describe('AttributeProcessor', () => {
         processor.pipe({
             processor: 'attribute',
             params: {
-                selectors: ['span'],
+                selectors: 'span',
                 action: 'delete',
                 attribute: 'attr1'
             }
@@ -89,6 +88,56 @@ describe('AttributeProcessor', () => {
             <span></span>
             <a attr2=""><img src="../src/sample.jpeg "></a>
             <span attr2=""></span>`)
+    })
+
+
+    it('adds attribute from element till top parent', () => {
+        const processor = new HtmlPipeline()
+        processor.pipe<AttributeProcessorParameters>({
+            processor: 'attribute',
+            params: {
+                selectors: 'img',
+                action: 'addToParents',
+                attribute: 'new-attr',
+                ifIn: 'body'
+            }
+        })
+        const result = processor.process(html)
+
+        expect(result).toBe(`<html>
+        <body new-attr="">
+            <div class="content" attr1=""></div>
+            <span attr1=""></span>
+            <a attr2="" new-attr=""><img src="../src/sample.jpeg "></a>
+            <span attr2=""></span>
+        </body>
+        </html>`)
+    })
+
+    it('append given value to attribute', () => {
+        const processor = new HtmlPipeline()
+        processor.pipe<AttributeProcessorParameters>({
+            processor: 'attribute',
+            params: {
+                selectors: ['div.content', 'span[attr1]'],
+                action: 'append',
+                attribute: {
+                    name: 'class',
+                    value: 'new-class'
+                },
+                ifIn: 'body'
+            }
+        })
+        const result = processor.process(html)
+
+        expect(result).toBe(`<html>
+        <body>
+            <div class="content new-class" attr1=""></div>
+            <span attr1="" class="new-class"></span>
+            <a attr2=""><img src="../src/sample.jpeg "></a>
+            <span attr2=""></span>
+        </body>
+        </html>`)
     })
 
     it('transforms attributes', () => {
--- src/processors/attribute-processor.ts
+++ src/processors/attribute-processor.ts
@@ -40,45 +40,78 @@ const transformActions = {
     }
 }
 
-type Attribute = { name: string, value: string }
+type Attribute = { name: string, value?: string }
 
 type TransformActionTypes = 'format' | 'trim' | 'joinUrl' | 'encodeUrl'
 
-type AttributeProcessorActions = 'add' | 'delete' | 'transform'
+type AttributeProcessorActions = 'add' | 'delete' | 'transform' | 'addToParents' | 'append'
 
-interface AttributeProcessorParameters {
+export interface AttributeProcessorParameters {
     selectors: string[] | string
     action: AttributeProcessorActions
     attribute: string | Attribute
     transformations?: { action: TransformActionTypes, params?: any[] }[]
+    ifIn?: string[] | string
+}
+
+function addAttribute(element: HtmlElement, attribute: string | Attribute) {
+    typeof attribute === 'string' ? 
+        element.attributes.setNamedItem({ name: attribute, value: '' }) :
+        element.attributes.setNamedItem({ name: attribute.name, value: attribute.value })
+}
+
+function removeAttribute(element: HtmlElement, attribute: string | Attribute) {
+    element.attributes.removeNamedItem(typeof attribute === 'string' ? attribute : attribute.name)
 }
 
 @Processor(HtmlProcessorTypes.Attribute)
 class AttributeProcessor implements HtmlProcessor<AttributeProcessorParameters> {
     private _selectors: string[]
+    private _ifIn: string[]
+    private _topParent: HtmlElement
 
     constructor(private _params: AttributeProcessorParameters) {
         this._selectors = Array.isArray(_params.selectors) ? _params.selectors : [_params.selectors]
+        this._ifIn = Array.isArray(this._params.ifIn) ? this._params.ifIn : (this._params.ifIn ? [this._params.ifIn] : null)
+        this._topParent = null
     }
     elementStarted(element: HtmlElement) {
         const params = this._params
-        if(this._selectors.reduce((prev, curr) => prev || element.matches(curr), false)) {
+        if(!this._topParent) {
+            this._topParent = this._ifIn === null || this._ifIn.reduce((prev, curr) => prev || element.matches(curr), false) ? element : null
+        }
+
+        if(this._topParent && this._selectors.reduce((prev, curr) => prev || element.matches(curr), false)) {
             if(params.action === 'add') {
-                typeof params.attribute === 'string' ? 
-                    element.attributes.setNamedItem({ name: params.attribute, value: '' }) :
-                    element.attributes.setNamedItem({ name: params.attribute.name, value: params.attribute.value })
+                addAttribute(element, params.attribute)
             } else if (params.action === 'delete') {
-                element.attributes.removeNamedItem(typeof params.attribute === 'string' ? params.attribute : params.attribute.name)
+                removeAttribute(element, params.attribute)
             } else if (params.action === 'transform') {
                 const attr = element.attributes.getNamedItem(typeof params.attribute === 'string' ? params.attribute : params.attribute.name)
                 for(const tranform of params.transformations) {
                     attr.value = transformActions[tranform.action].apply(null, [attr.value, ...(tranform.params || [])])
                 }
+            } else if (params.action === 'addToParents') {
+                for(let current = element.parentElement; current; current = current.parentElement) {
+                    addAttribute(current, params.attribute)
+                    if(current === this._topParent) break
+                }
+            } else if (params.action === 'append') {
+                if(typeof params.attribute === 'string' || !params.attribute.value) throw new Error('Given attribute parameter doesnt have value.')
+                const attr = element.attributes.getNamedItem(params.attribute.name)
+                if(attr) {
+                    attr.value += ` ${params.attribute.value}`
+                    element.attributes.setNamedItem(attr)
+                } else {
+                    element.attributes.setNamedItem({ name: params.attribute.name, value: params.attribute.value })
+                }
             }
         }
     }
     elementEnded(element: HtmlElement) {
-        
+        if(this._topParent === element) {
+            this._topParent = null
+        }
     }
     textCreated(node: HtmlNode) {
         
